de99bec98b48d7759fd3a8053ad78d7b836ad55b
Tobias Hotz <tobias.hotz@hotmail.de>
2022-12-23 20:39:50 +0100

    Upgrade to 1.19.2

---
+++ src/main/java/ichttt/mods/eternalwinter/BiomesModifier.java
@@ -0,0 +1,44 @@
+package ichttt.mods.eternalwinter;
+
+import com.mojang.serialization.Codec;
+import net.minecraft.core.Holder;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.level.biome.Biome;
+import net.minecraftforge.common.world.BiomeModifier;
+import net.minecraftforge.common.world.ModifiableBiomeInfo;
+
+public class BiomesModifier implements BiomeModifier {
+
+    @Override
+    public void modify(Holder<Biome> biome, Phase phase, ModifiableBiomeInfo.BiomeInfo.Builder builder) {
+        if (phase == Phase.MODIFY) {
+            ResourceLocation registryName = biome.unwrapKey().get().location();
+            if (EWConfig.GENERAL.listMode.get() == ListMode.BLACKLIST) {
+                if (EWConfig.GENERAL.biomeList.get().stream().anyMatch(s -> s.equals(registryName.getPath()) || s.equals(registryName.toString())))
+                    return;
+            } else if (EWConfig.GENERAL.listMode.get() == ListMode.WHITELIST) {
+                if (EWConfig.GENERAL.biomeList.get().stream().noneMatch(s -> s.equals(registryName.getPath()) || s.equals(registryName.toString())))
+                    return;
+            } else {
+                throw new RuntimeException("Wrong list mode! " + EWConfig.GENERAL.biomeList.get());
+            }
+
+            float downfall = EWConfig.GENERAL.downfall.get().floatValue();
+            if (downfall != -1 && downfall < 0) {
+                EternalWinter.LOGGER.error("Invalid downfall " + downfall + ", defaulting to -1!");
+                downfall = -1F;
+            }
+            if (downfall != -1F)
+                builder.getClimateSettings().setDownfall(downfall);
+            builder.getClimateSettings().setTemperature(0F);
+            builder.getClimateSettings().setTemperatureModifier(Biome.TemperatureModifier.NONE);
+            builder.getClimateSettings().setPrecipitation(Biome.Precipitation.SNOW);
+            EternalWinter.LOGGER.debug("Modified Biome {} successful!", registryName);
+        }
+    }
+
+    @Override
+    public Codec<? extends BiomeModifier> codec() {
+        return EternalWinter.BIOME_MODIFIER_CODEC.get();
+    }
+}
--- src/main/java/ichttt/mods/eternalwinter/EternalWinter.java
+++ src/main/java/ichttt/mods/eternalwinter/EternalWinter.java
@@ -1,47 +1,29 @@
 package ichttt.mods.eternalwinter;
 
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.level.biome.Biome;
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.world.BiomeLoadingEvent;
-import net.minecraftforge.eventbus.api.EventPriority;
+import com.mojang.serialization.Codec;
+import net.minecraftforge.common.world.BiomeModifier;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 @Mod("eternalwinter")
 public class EternalWinter {
-    private static final Logger LOGGER = LogManager.getLogger("EternalWinter");
+    public static final Logger LOGGER = LogManager.getLogger("EternalWinter");
+
+    private static final DeferredRegister<Codec<? extends BiomeModifier>> BIOME_MODIFIER_SERIALIZERS = DeferredRegister.create(ForgeRegistries.Keys.BIOME_MODIFIER_SERIALIZERS, "eternalwinter");
+    public static final RegistryObject<Codec<BiomesModifier>> BIOME_MODIFIER_CODEC = BIOME_MODIFIER_SERIALIZERS.register("change_to_snow", () -> Codec.unit(BiomesModifier::new));
 
     public EternalWinter() {
         LOGGER.info("Starting up eternal winter mod");
-        MinecraftForge.EVENT_BUS.addListener(EventPriority.LOW, this::onLoadComplete);
         ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, EWConfig.generalSpec);
-    }
-
-    public void onLoadComplete(BiomeLoadingEvent event) { //All biomes should be known by now
-        if (EWConfig.GENERAL.listMode.get() == ListMode.BLACKLIST) {
-            ResourceLocation registryName = event.getName();
-            if (EWConfig.GENERAL.biomeList.get().stream().anyMatch(s -> s.equals(registryName.getPath()) || s.equals(registryName.toString())))
-                return;
-        } else if (EWConfig.GENERAL.listMode.get() == ListMode.WHITELIST) {
-            ResourceLocation registryName = event.getName();
-            if (EWConfig.GENERAL.biomeList.get().stream().noneMatch(s -> s.equals(registryName.getPath()) || s.equals(registryName.toString())))
-                return;
-        } else {
-            throw new RuntimeException("Wrong list mode! " + EWConfig.GENERAL.biomeList.get());
-        }
-
-        float downfall = EWConfig.GENERAL.downfall.get().floatValue();
-        if (downfall != -1 && downfall < 0) {
-            LOGGER.error("Invalid downfall " + downfall + ", defaulting to -1!");
-            downfall = -1F;
-        }
-        if (downfall == -1F)
-            downfall = event.getClimate().downfall;
-        event.setClimate(new Biome.ClimateSettings(Biome.Precipitation.SNOW, 0F, Biome.TemperatureModifier.NONE, downfall));
-        LOGGER.debug("Modified Biome {} successful!", event.getName());
+        final IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+        BIOME_MODIFIER_SERIALIZERS.register(modEventBus);
     }
 }
