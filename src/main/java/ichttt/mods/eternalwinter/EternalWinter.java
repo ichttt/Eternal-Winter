69d5cf9b98456794b5d1508ee0522c5ca851e20c
Tobias Hotz <tobias.hotz@hotmail.de>
2023-08-18 21:31:48 +0200

    Set network ignore flag

--- src/main/java/ichttt/mods/eternalwinter/EternalWinter.java
+++ src/main/java/ichttt/mods/eternalwinter/EternalWinter.java
@@ -3,6 +3,7 @@ package ichttt.mods.eternalwinter;
 import com.mojang.serialization.Codec;
 import net.minecraftforge.common.world.BiomeModifier;
 import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.IExtensionPoint;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
@@ -25,5 +26,8 @@ public class EternalWinter {
         ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, EWConfig.generalSpec);
         final IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
         BIOME_MODIFIER_SERIALIZERS.register(modEventBus);
+
+        //Make sure the mod being absent on the other network side does not cause the client to display the server as incompatible
+        ModLoadingContext.get().registerExtensionPoint(IExtensionPoint.DisplayTest.class, () -> new IExtensionPoint.DisplayTest(() -> IExtensionPoint.DisplayTest.IGNORESERVERONLY, (a, b) -> true));
     }
 }
